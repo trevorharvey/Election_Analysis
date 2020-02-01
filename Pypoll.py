bea4bd4142d87232a3171c6fb03c340f593bbcc8
Cryptotea <cryptotea@Cryptoteas-MacBook-Air.local>
2020-01-31 19:01:12 -0800

    Adding Pypoll.py file.

---
+++ Pypoll.py
@@ -0,0 +1,22 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast.
+# 2. A complete list of candidates who recieved votes.
+# 3. The percentage of votes each candidate won.
+# 4. The total number of votes each candidate won.
+# 5. The winner of the election based on popular vote.
+
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join('Election_Resources', 'election_results.csv')
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("Election_Analysis","election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
