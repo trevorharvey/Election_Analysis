39f7e2e8133385bbaf76e39849725cc13e5ee957
trevorharvey <59985505+trevorharvey@users.noreply.github.com>
2020-02-02 15:35:54 -0800

    Delete Python_practice.py

--- Python_practice.py
+++
@@ -1,7 +0,0 @@
-counties_dict = {"Arapahoe":42289,"Denver":463353,"Jefferson":432438}
-for county in counties_dict:
-    print(county)
-for voters in counties_dict.values():
-    print(voters)
-for county, voters in counties_dict.items():
-    print(county, voters)
